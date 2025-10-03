1755ef2cb19825b7f2f3dd7f5f91bdcc504861f9
Nastya E <dplu99ed@gmail.com>
2025-10-04 04:04:03 +0500

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import react from '@vitejs/plugin-react'
-
+import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +8,7 @@ export default defineConfig({
   base: '/Coffee-Listing/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'src'),                 // <— главный пункт
     },
   },
 })
