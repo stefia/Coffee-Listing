bfe51b72f3504ba4071d561a022b4c0957c95f15
Nastya E <dplu99ed@gmail.com>
2025-10-03 23:49:50 +0500

    ...

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Coffee-Listing/'
+  base: '/Coffee-Listing/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
